f952841d4e2181ca93fb412c278abe0623f246c4
Mariano Barrios <marbar@gmail.com>
2016-01-16 12:04:19 +0100

    Separate BlockingTest into BlockingTest and InteroperabilityTest

--- src/test/scala/tlschannel/BlockingTest.scala
+++ src/test/scala/tlschannel/BlockingTest.scala
@@ -7,16 +7,17 @@ import java.io.IOException
 import TestUtil.functionToRunnable
 import javax.net.ssl.SSLContext
 import javax.net.ssl.SSLSocketFactory
+import java.nio.channels.ByteChannel
+import java.nio.ByteBuffer
 
 class BlockingTest extends FunSuite with Matchers {
 
-  import SocketWrappers._
-    
+  val factory = new SocketPairFactory(7777)
+
   val dataSize = 10 * 1024 * 1024 + Random.nextInt(10000)
+  
   val data = Array.ofDim[Byte](dataSize)
   Random.nextBytes(data)
-
-  val margin = Random.nextInt(100)
 
   val sslEngine = SSLContext.getDefault.createSSLEngine()
 
@@ -26,176 +27,74 @@ class BlockingTest extends FunSuite with Matchers {
     // not using authentication
     .filter(_.contains("_anon_"))
 
-  def writerLoop(writer: Writer, idx: Int, renegotiate: Boolean = false) = TestUtil.cannotFail(s"Error in writer $idx") {
-    var remaining = dataSize
-    while (remaining > 0) {
+  def writerLoop(writer: ByteChannel, rawWriter: TlsSocketChannel, renegotiate: Boolean = false): Unit = TestUtil.cannotFail("Error in writer") {
+    val originData = ByteBuffer.wrap(data)
+    while (originData.hasRemaining) {
       if (renegotiate)
-        writer.renegotiate()
-      val chunkSize = Random.nextInt(remaining) + 1 // 1 <= chunkSize <= remaining
-      writer.write(data, dataSize - remaining, chunkSize)
-      remaining -= chunkSize
+        rawWriter.renegotiate()
+      val c = writer.write(originData)
+      assert(c > 0)
     }
   }
 
-  def readerLoop(reader: Reader, idx: Int = 0) = TestUtil.cannotFail(s"Error in reader $idx") {
-    val receivedData = Array.ofDim[Byte](dataSize + margin)
-    var remaining = dataSize
-    while (remaining > 0) {
-      val chunkSize = Random.nextInt(remaining + margin) + 1 // 1 <= chunkSize <= remaining + margin
-      val c = reader.read(receivedData, dataSize - remaining, chunkSize)
-      assert(c != -1, "read must not return -1 when there were bytes remaining")
-      assert(c <= remaining)
+  def readerLoop(reader: ByteChannel): Unit = TestUtil.cannotFail("Error in reader") {
+    val receivedData = ByteBuffer.allocate(dataSize)
+    while (receivedData.hasRemaining) {
+      val c = reader.read(receivedData)
       assert(c > 0, "blocking read must return a positive number")
-      remaining -= c
     }
-    assert(remaining == 0)
-    assert(receivedData.slice(0, dataSize).deep === data.deep)
-  }
-
-  /**
-   * Test a simplex (one-direction) interaction, with random renegotiations interleaved with the data
-   */
-  def simplexStream(cipher: String, writer: Writer, reader: Reader) {
-    val (_, elapsed) = TestUtil.time {
-      val writerThread = new Thread(() => writerLoop(writer, idx = 0, renegotiate = true), "writer")
-      val readerThread = new Thread(() => readerLoop(reader), "reader")
-      Seq(readerThread, writerThread).foreach(_.start())
-      Seq(readerThread, writerThread).foreach(_.join())
-      writer.close()
-      reader.close()
-    }
-    info(s"elapsed for $cipher: ${elapsed / 1000} ms")
+    assert(receivedData.array.deep === data.deep)
   }
 
   /**
    * Test a half-duplex interaction, with renegotiation before reversing the direction of the flow (as in HTTP)
    */
-  def halfDuplexStream(
-    idx: Int, cipher: String, serverWriter: Writer, clientReader: Reader, clientWriter: Writer, serverReader: Reader) {
-    val (_, elapsed) = TestUtil.time {
-      val clientWriterThread = new Thread(() => writerLoop(clientWriter, idx), s"client-writer-$idx")
-      val serverWriterThread = new Thread(() => writerLoop(serverWriter, idx), s"server-writer-$idx")
-      val clientReaderThread = new Thread(() => readerLoop(clientReader, idx), s"client-reader-$idx")
-      val serverReaderThread = new Thread(() => readerLoop(serverReader, idx), s"server-reader-$idx")
-      Seq(serverReaderThread, clientWriterThread).foreach(_.start())
-      Seq(serverReaderThread, clientWriterThread).foreach(_.join())
-      clientReaderThread.start()
-      // renegotiate three times, to test idempotency
-      for (_ <- 1 to 3) {
-        serverWriter.renegotiate()
+  test("half duplex") {
+    for (cipher <- ciphers) {
+      withClue(cipher + ": ") {
+        val ((client, _), (server, _)) = factory.nioNio(cipher)
+        val (_, elapsed) = TestUtil.time {
+          val clientWriterThread = new Thread(() => writerLoop(new ChunkingByteChannel(client), client, renegotiate = true), "client-writer")
+          val serverWriterThread = new Thread(() => writerLoop(new ChunkingByteChannel(server), server, renegotiate = true), "server-writer")
+          val clientReaderThread = new Thread(() => readerLoop(new ChunkingByteChannel(client)), "client-reader")
+          val serverReaderThread = new Thread(() => readerLoop(new ChunkingByteChannel(server)), "server-reader")
+          Seq(serverReaderThread, clientWriterThread).foreach(_.start())
+          Seq(serverReaderThread, clientWriterThread).foreach(_.join())
+          clientReaderThread.start()
+          // renegotiate three times, to test idempotency
+          for (_ <- 1 to 3) {
+            server.renegotiate()
+          }
+          serverWriterThread.start()
+          Seq(clientReaderThread, serverWriterThread).foreach(_.join())
+          server.close()
+          client.close()
+        }
+        info(s"elapsed for $cipher: ${elapsed / 1000} ms")
       }
-      serverWriterThread.start()
-      Seq(clientReaderThread, serverWriterThread).foreach(_.join())
-      serverWriter.close()
-      clientWriter.close()
     }
-    info(s"elapsed for $cipher: ${elapsed / 1000} ms")
   }
 
   /**
    * Test a full-duplex interaction, without any renegotiation
    */
-  def fullDuplexStream(
-    idx: Int, cipher: String, serverWriter: Writer, clientReader: Reader, clientWriter: Writer, serverReader: Reader) {
-    val (_, elapsed) = TestUtil.time {
-      val clientWriterThread = new Thread(() => writerLoop(clientWriter, idx), s"client-writer-$idx")
-      val serverWriterThread = new Thread(() => writerLoop(serverWriter, idx), s"server-writer-$idx")
-      val clientReaderThread = new Thread(() => readerLoop(clientReader, idx), s"client-reader-$idx")
-      val serverReaderThread = new Thread(() => readerLoop(serverReader, idx), s"server-reader-$idx")
-      Seq(serverReaderThread, clientWriterThread, clientReaderThread, serverWriterThread).foreach(_.start())
-      Seq(serverReaderThread, clientWriterThread, clientReaderThread, serverWriterThread).foreach(_.join())
-      clientWriter.close()
-      serverWriter.close()
-    }
-    info(s"elapsed for $cipher: ${elapsed / 1000} ms")
-  }
- 
-  // NIO -> OLD IO    
-
-  test("nio -> old-io (simplex)") {
+  test("full duplex") {
     for (cipher <- ciphers) {
       withClue(cipher + ": ") {
-        val ((clientWriter, _), (_, serverReader)) = nioOld(cipher)
-        simplexStream(cipher, clientWriter, serverReader)
+        val ((client, _), (server, _)) = factory.nioNio(cipher)
+        val (_, elapsed) = TestUtil.time {
+          val clientWriterThread = new Thread(() => writerLoop(new ChunkingByteChannel(client), client), "client-writer")
+          val serverWriterThread = new Thread(() => writerLoop(new ChunkingByteChannel(server), server), "server-write")
+          val clientReaderThread = new Thread(() => readerLoop(new ChunkingByteChannel(client)), "client-reader")
+          val serverReaderThread = new Thread(() => readerLoop(new ChunkingByteChannel(server)), "server-reader")
+          Seq(serverReaderThread, clientWriterThread, clientReaderThread, serverWriterThread).foreach(_.start())
+          Seq(serverReaderThread, clientWriterThread, clientReaderThread, serverWriterThread).foreach(_.join())
+          client.close()
+          server.close()
+        }
+        info(s"elapsed for $cipher: ${elapsed / 1000} ms")
       }
     }
   }
-
-  test("nio -> old-io (half duplex)") {
-    for ((cipher, idx) <- ciphers.zipWithIndex) {
-      withClue(cipher + ": ") {
-        val ((clientWriter, clientReader), (serverWriter, serverReader)) = nioOld(cipher)
-        halfDuplexStream(idx, cipher, serverWriter, clientReader, clientWriter, serverReader)
-      }
-    }
-  }
-
-  test("nio -> old-io (full duplex)") {
-    for ((cipher, idx) <- ciphers.zipWithIndex) {
-      withClue(cipher + ": ") {
-        val ((clientWriter, clientReader), (serverWriter, serverReader)) = nioOld(cipher)
-        fullDuplexStream(idx, cipher, serverWriter, clientReader, clientWriter, serverReader)
-      }
-    }
-  }
-
-  // OLD IO -> NIO    
-
-  test("old-io -> nio (simplex)") {
-    for (cipher <- ciphers) {
-      withClue(cipher + ": ") {
-        val ((clientWriter, _), (_, serverReader)) = oldNio(cipher)
-        simplexStream(cipher, clientWriter, serverReader)
-      }
-    }
-  }
-
-  test("tls: old-io -> nio (half duplex)") {
-    for ((cipher, idx) <- ciphers.zipWithIndex) {
-      withClue(cipher + ": ") {
-        val ((clientWriter, clientReader), (serverWriter, serverReader)) = oldNio(cipher)
-        halfDuplexStream(idx, cipher, serverWriter, clientReader, clientWriter, serverReader)
-      }
-    }
-  }
-
-  test("old-io -> nio (full duplex)") {
-    for ((cipher, idx) <- ciphers.zipWithIndex) {
-      withClue(cipher + ": ") {
-        val ((clientWriter, clientReader), (serverWriter, serverReader)) = oldNio(cipher)
-        fullDuplexStream(idx, cipher, serverWriter, clientReader, clientWriter, serverReader)
-      }
-    }
-  }
-
-  // NIO -> NIO
-
-  test("nio -> nio (simplex)") {
-    for (cipher <- ciphers) {
-      withClue(cipher + ": ") {
-        val ((clientWriter, _), (_, serverReader)) = nioNio(cipher)
-        simplexStream(cipher, clientWriter, serverReader)
-      }
-    }
-  }
-
-  test("nio -> nio (half duplex)") {
-    for (cipher <- ciphers) {
-      withClue(cipher + ": ") {
-        val ((clientWriter, clientReader), (serverWriter, serverReader)) = nioNio(cipher)
-        halfDuplexStream(0, cipher, serverWriter, clientReader, clientWriter, serverReader)
-      }
-    }
-  }
-
-  test("nio -> nio (full duplex)") {
-    for (cipher <- ciphers) {
-      withClue(cipher + ": ") {
-        val ((clientWriter, clientReader), (serverWriter, serverReader)) = nioNio(cipher)
-        fullDuplexStream(0, cipher, serverWriter, clientReader, clientWriter, serverReader)
-      }
-    }
-  }
-
 
 }
---
+++ src/test/scala/tlschannel/InteroperabilityTest.scala
@@ -0,0 +1,205 @@
+package tlschannel
+
+import org.scalatest.FunSuite
+import org.scalatest.Matchers
+import scala.util.Random
+import java.io.IOException
+import TestUtil.functionToRunnable
+import javax.net.ssl.SSLContext
+import javax.net.ssl.SSLSocketFactory
+import java.net.Socket
+import java.nio.channels.ByteChannel
+import javax.net.ssl.SSLSocket
+import java.nio.ByteBuffer
+
+class InteroperabilityTest extends FunSuite with Matchers {
+
+  trait Reader {
+    def read(array: Array[Byte], offset: Int, length: Int): Int
+    def close(): Unit
+  }
+
+  class SocketReader(socket: Socket) extends Reader {
+    private val is = socket.getInputStream
+    def read(array: Array[Byte], offset: Int, length: Int) = is.read(array, offset, length)
+    def close() = socket.close()
+  }
+
+  class ByteChannelReader(socket: ByteChannel) extends Reader with Matchers {
+    def read(array: Array[Byte], offset: Int, length: Int) = socket.read(ByteBuffer.wrap(array, offset, length))
+    def close() = socket.close()
+  }
+
+  trait Writer {
+    def renegotiate(): Unit
+    def write(array: Array[Byte], offset: Int, length: Int): Unit
+    def close(): Unit
+  }
+
+  class SSLSocketWriter(socket: SSLSocket) extends Writer {
+    private val os = socket.getOutputStream
+    def write(array: Array[Byte], offset: Int, length: Int) = os.write(array, offset, length)
+    def renegotiate() = socket.startHandshake()
+    def close() = socket.close()
+  }
+
+  class TlsSocketChannelWriter(val socket: TlsSocketChannel) extends Writer with Matchers {
+
+    def write(array: Array[Byte], offset: Int, length: Int) = {
+      val buffer = ByteBuffer.wrap(array, offset, length)
+      while (buffer.remaining() > 0) {
+        val c = socket.write(buffer)
+        assert(c != 0, "blocking write cannot return 0")
+      }
+    }
+
+    def renegotiate(): Unit = socket.renegotiate()
+    def close() = socket.close()
+
+  }
+
+  val factory = new SocketPairFactory(7777)
+
+  def nioNio(cipher: String) = {
+    val ((client, _), (server, _)) = factory.nioNio(cipher)
+    val clientPair = (new TlsSocketChannelWriter(client), new ByteChannelReader(client))
+    val serverPair = (new TlsSocketChannelWriter(server), new ByteChannelReader(server))
+    (clientPair, serverPair)
+  }
+
+  def oldNio(cipher: String) = {
+    val (client, (server, _)) = factory.oldNio(cipher)
+    val clientPair = (new SSLSocketWriter(client), new SocketReader(client))
+    val serverPair = (new TlsSocketChannelWriter(server), new ByteChannelReader(server))
+    (clientPair, serverPair)
+  }
+
+  def nioOld(cipher: String) = {
+    val ((client, _), server) = factory.nioOld(cipher)
+    val clientPair = (new TlsSocketChannelWriter(client), new ByteChannelReader(client))
+    val serverPair = (new SSLSocketWriter(server), new SocketReader(server))
+    (clientPair, serverPair)
+  }
+
+  val dataSize = 10 * 1024 * 1024 + Random.nextInt(10000)
+  val data = Array.ofDim[Byte](dataSize)
+  Random.nextBytes(data)
+
+  val margin = Random.nextInt(100)
+
+  val sslEngine = SSLContext.getDefault.createSSLEngine()
+
+  val ciphers = sslEngine.getSupportedCipherSuites
+    // Java 8 disabled SSL through another mechanism, ignore that protocol here, to avoid errors 
+    .filter(_.startsWith("TLS_"))
+    // not using authentication
+    .filter(_.contains("_anon_"))
+
+  def writerLoop(writer: Writer, renegotiate: Boolean = false) = TestUtil.cannotFail("Error in writer") {
+    var remaining = dataSize
+    while (remaining > 0) {
+      if (renegotiate)
+        writer.renegotiate()
+      val chunkSize = Random.nextInt(remaining) + 1 // 1 <= chunkSize <= remaining
+      writer.write(data, dataSize - remaining, chunkSize)
+      remaining -= chunkSize
+    }
+  }
+
+  def readerLoop(reader: Reader, idx: Int = 0) = TestUtil.cannotFail("Error in reader") {
+    val receivedData = Array.ofDim[Byte](dataSize + margin)
+    var remaining = dataSize
+    while (remaining > 0) {
+      val chunkSize = Random.nextInt(remaining + margin) + 1 // 1 <= chunkSize <= remaining + margin
+      val c = reader.read(receivedData, dataSize - remaining, chunkSize)
+      assert(c != -1, "read must not return -1 when there were bytes remaining")
+      assert(c <= remaining)
+      assert(c > 0, "blocking read must return a positive number")
+      remaining -= c
+    }
+    assert(remaining == 0)
+    assert(receivedData.slice(0, dataSize).deep === data.deep)
+  }
+
+  /**
+   * Test a half-duplex interaction, with renegotiation before reversing the direction of the flow (as in HTTP)
+   */
+  def halfDuplexStream(cipher: String, serverWriter: Writer, clientReader: Reader, clientWriter: Writer, serverReader: Reader) {
+    val (_, elapsed) = TestUtil.time {
+      val clientWriterThread = new Thread(() => writerLoop(clientWriter, renegotiate = true), "client-writer")
+      val serverWriterThread = new Thread(() => writerLoop(serverWriter, renegotiate = true), "server-writer")
+      val clientReaderThread = new Thread(() => readerLoop(clientReader), "client-reader")
+      val serverReaderThread = new Thread(() => readerLoop(serverReader), "server-reader")
+      Seq(serverReaderThread, clientWriterThread).foreach(_.start())
+      Seq(serverReaderThread, clientWriterThread).foreach(_.join())
+      clientReaderThread.start()
+      // renegotiate three times, to test idempotency
+      for (_ <- 1 to 3) {
+        serverWriter.renegotiate()
+      }
+      serverWriterThread.start()
+      Seq(clientReaderThread, serverWriterThread).foreach(_.join())
+      serverWriter.close()
+      clientWriter.close()
+    }
+    info(s"elapsed for $cipher: ${elapsed / 1000} ms")
+  }
+
+  /**
+   * Test a full-duplex interaction, without any renegotiation
+   */
+  def fullDuplexStream(cipher: String, serverWriter: Writer, clientReader: Reader, clientWriter: Writer, serverReader: Reader) {
+    val (_, elapsed) = TestUtil.time {
+      val clientWriterThread = new Thread(() => writerLoop(clientWriter), "client-writer")
+      val serverWriterThread = new Thread(() => writerLoop(serverWriter), "server-writer")
+      val clientReaderThread = new Thread(() => readerLoop(clientReader), "client-reader")
+      val serverReaderThread = new Thread(() => readerLoop(serverReader), "server-reader")
+      Seq(serverReaderThread, clientWriterThread, clientReaderThread, serverWriterThread).foreach(_.start())
+      Seq(serverReaderThread, clientWriterThread, clientReaderThread, serverWriterThread).foreach(_.join())
+      clientWriter.close()
+      serverWriter.close()
+    }
+    info(s"elapsed for $cipher: ${elapsed / 1000} ms")
+  }
+
+  // NIO -> OLD IO    
+
+  test("nio -> old-io (half duplex)") {
+    for ((cipher) <- ciphers) {
+      withClue(cipher + ": ") {
+        val ((clientWriter, clientReader), (serverWriter, serverReader)) = nioOld(cipher)
+        halfDuplexStream(cipher, serverWriter, clientReader, clientWriter, serverReader)
+      }
+    }
+  }
+
+  test("nio -> old-io (full duplex)") {
+    for ((cipher) <- ciphers) {
+      withClue(cipher + ": ") {
+        val ((clientWriter, clientReader), (serverWriter, serverReader)) = nioOld(cipher)
+        fullDuplexStream(cipher, serverWriter, clientReader, clientWriter, serverReader)
+      }
+    }
+  }
+
+  // OLD IO -> NIO    
+
+  test("old-io -> nio (half duplex)") {
+    for ((cipher, idx) <- ciphers.zipWithIndex) {
+      withClue(cipher + ": ") {
+        val ((clientWriter, clientReader), (serverWriter, serverReader)) = oldNio(cipher)
+        halfDuplexStream(cipher, serverWriter, clientReader, clientWriter, serverReader)
+      }
+    }
+  }
+
+  test("old-io -> nio (full duplex)") {
+    for ((cipher, idx) <- ciphers.zipWithIndex) {
+      withClue(cipher + ": ") {
+        val ((clientWriter, clientReader), (serverWriter, serverReader)) = oldNio(cipher)
+        fullDuplexStream(cipher, serverWriter, clientReader, clientWriter, serverReader)
+      }
+    }
+  }
+
+}
