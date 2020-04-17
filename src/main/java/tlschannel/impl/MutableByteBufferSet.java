8c36fa594ee36a6323d7b31024c7333bf785b05e
Stefanos Zachariadis <stefanos@zachariadis.net>
2020-04-17 12:29:09 +0100

    [Stef] introduce mutable byte buffer set

---
+++ src/main/java/tlschannel/impl/MutableByteBufferSet.java
@@ -0,0 +1,117 @@
+package tlschannel.impl;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+import javax.net.ssl.SSLEngine;
+import javax.net.ssl.SSLEngineResult;
+import javax.net.ssl.SSLException;
+import tlschannel.TlsChannel;
+
+public class MutableByteBufferSet implements ByteBufferSet {
+  private ByteBuffer[] buffers;
+  private int offset;
+  private int length;
+
+  public MutableByteBufferSet wrap(ByteBuffer[] buffers, int offset, int length) {
+    if (buffers == null) throw new NullPointerException();
+    if (buffers.length < offset) throw new IndexOutOfBoundsException();
+    if (buffers.length < offset + length) throw new IndexOutOfBoundsException();
+    for (int i = offset; i < offset + length; i++) {
+      if (buffers[i] == null) throw new NullPointerException();
+    }
+    this.buffers = buffers;
+    this.offset = offset;
+    this.length = length;
+    return this;
+  }
+
+  @Override
+  public long remaining() {
+    return ByteBufferSetUtil.remaining(buffers, offset, length);
+  }
+
+  @Override
+  public int putRemaining(ByteBuffer from) {
+    return ByteBufferSetUtil.putRemaining(from, buffers, offset, length);
+  }
+
+  @Override
+  public ByteBufferSet put(ByteBuffer from, int length) {
+    ByteBufferSetUtil.put(from, length, buffers, offset, this.length, remaining());
+    return this;
+  }
+
+  @Override
+  public int getRemaining(ByteBuffer dst) {
+    return ByteBufferSetUtil.getRemaining(dst, buffers, offset, length);
+  }
+
+  @Override
+  public ByteBufferSet get(ByteBuffer dst, int length) {
+    ByteBufferSetUtil.get(dst, length, remaining(), buffers, offset, this.length);
+    return this;
+  }
+
+  @Override
+  public boolean hasRemaining() {
+    return remaining() > 0;
+  }
+
+  @Override
+  public boolean isReadOnly() {
+    return ByteBufferSetUtil.isReadOnly(buffers, offset, length);
+  }
+
+  @Override
+  public SSLEngineResult unwrap(final SSLEngine engine, final ByteBuffer buffer)
+      throws SSLException {
+    if (numberOfBuffersRemaining() == 1) {
+      return engine.unwrap(buffer, buffers[offset]);
+    } else {
+      return engine.unwrap(buffer, buffers, offset, length);
+    }
+  }
+
+  @Override
+  public SSLEngineResult wrap(final SSLEngine engine, final ByteBuffer buffer) throws SSLException {
+    if (numberOfBuffersRemaining() == 1) {
+      return engine.wrap(buffers[offset], buffer);
+    } else {
+      return engine.wrap(buffers, offset, length, buffer);
+    }
+  }
+
+  @Override
+  public long read(final TlsChannel tlsChannel) throws IOException {
+    if (numberOfBuffersRemaining() == 1) {
+      return tlsChannel.read(buffers[offset]);
+    } else {
+      return tlsChannel.read(buffers, offset, length);
+    }
+  }
+
+  @Override
+  public void write(final TlsChannel tlsChannel) throws IOException {
+    if (numberOfBuffersRemaining() == 1) {
+      tlsChannel.write(buffers[offset]);
+    } else {
+      tlsChannel.write(buffers, offset, length);
+    }
+  }
+
+  @Override
+  public String toString() {
+    return "ImmutableByteBufferSet[buffers="
+        + Arrays.toString(buffers)
+        + ", offset="
+        + offset
+        + ", length="
+        + length
+        + "]";
+  }
+
+  private int numberOfBuffersRemaining() {
+    return length - offset;
+  }
+}
