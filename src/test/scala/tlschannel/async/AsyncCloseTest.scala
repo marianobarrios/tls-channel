5a157afb59164608f46cada58a56df41987adfba
Mariano Barrios <marbar@gmail.com>
2021-08-08 13:45:07 +0200

    Remove racy test

--- src/test/scala/tlschannel/async/AsyncCloseTest.scala
+++ src/test/scala/tlschannel/async/AsyncCloseTest.scala
@@ -23,7 +23,7 @@ class AsyncCloseTest extends AnyFunSuite with AsyncTestBase with Assertions {
    * Closing sockets registered in an asynchronous channel group is inherently racy, using repetitions to try to catch
    * most races.
    */
-  val repetitions = 500
+  val repetitions = 100
 
   test("should throw an CancellationException (or ClosedChannelException) when closing the group while reading") {
     for (_ <- 1 to repetitions) {
@@ -35,8 +35,6 @@ class AsyncCloseTest extends AnyFunSuite with AsyncTestBase with Assertions {
 
       socketPair.server.external.close()
       socketPair.client.external.close()
-
-      Thread.sleep(10)
 
       intercept[Exception] {
         readFuture.get(1000, TimeUnit.MILLISECONDS)
@@ -55,40 +53,6 @@ class AsyncCloseTest extends AnyFunSuite with AsyncTestBase with Assertions {
       assertChannelGroupConsistency(channelGroup)
 
       assert(channelGroup.getSuccessfulReadCount == 0)
-
-      printChannelGroupStatus(channelGroup)
-    }
-  }
-
-  test("should throw an CancellationException (or ClosedChannelException) when closing the group while writing") {
-    for (_ <- 1 to repetitions) {
-      val channelGroup = new AsynchronousTlsChannelGroup()
-      val socketPair = factory.async(null, channelGroup, runTasks = true)
-
-      val writeBuffer = ByteBuffer.allocate(bufferSize)
-      val writeFuture = socketPair.server.external.write(writeBuffer)
-      socketPair.server.external.close()
-      socketPair.client.external.close()
-
-      Thread.sleep(200)
-
-      intercept[Exception] {
-        writeFuture.get(1000, TimeUnit.MILLISECONDS)
-      } match {
-        case _: CancellationException =>
-          assert(channelGroup.getCancelledWriteCount == 1)
-          assert(channelGroup.getFailedWriteCount == 0)
-        case ee: ExecutionException =>
-          assert(ee.getCause.isInstanceOf[ClosedChannelException])
-          assert(channelGroup.getCancelledWriteCount == 0)
-          assert(channelGroup.getFailedWriteCount == 1)
-        case e => fail(e)
-      }
-
-      shutdownChannelGroup(channelGroup)
-      assertChannelGroupConsistency(channelGroup)
-
-      assert(channelGroup.getSuccessfulWriteCount == 0)
 
       printChannelGroupStatus(channelGroup)
     }
