f5a4914de64bd0dd111136fcfafe0836bda49e12
Mariano Barrios <marbar@gmail.com>
2023-11-26 13:31:34 +0100

    Migrato to Java: ScatteringTest

---
+++ src/test/scala/tlschannel/ScatteringTest.java
@@ -0,0 +1,25 @@
+package tlschannel;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import scala.Option;
+import tlschannel.helpers.Loops;
+import tlschannel.helpers.SocketPair;
+import tlschannel.helpers.SocketPairFactory;
+import tlschannel.helpers.SslContextFactory;
+
+@TestInstance(Lifecycle.PER_CLASS)
+public class ScatteringTest {
+
+    private final SslContextFactory sslContextFactory = new SslContextFactory();
+    private final SocketPairFactory factory = new SocketPairFactory(sslContextFactory.defaultContext());
+
+    private static final int dataSize = 150 * 1000;
+
+    @Test
+    public void testHalfDuplex() {
+        SocketPair socketPair = factory.nioNio(Option.apply(null), Option.apply(null), true, false, Option.apply(null));
+        Loops.halfDuplex(socketPair, dataSize, true, false);
+    }
+}
