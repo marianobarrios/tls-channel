723cbc7fe5f8931aa7cbce5c4c010d57d3a347c0
Mariano Barrios <marbar@gmail.com>
2024-03-31 19:01:17 +0200

    Migrate to Java: NullMultiNonBlockingTest

---
+++ src/test/scala/tlschannel/NullMultiNonBlockingTest.java
@@ -0,0 +1,38 @@
+package tlschannel;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import scala.Option;
+import scala.collection.immutable.Seq;
+import tlschannel.helpers.NonBlockingLoops;
+import tlschannel.helpers.SocketPair;
+import tlschannel.helpers.SocketPairFactory;
+import tlschannel.helpers.SslContextFactory;
+
+/** Test using concurrent, non-blocking connections, and a "null" [[javax.net.ssl.SSLEngine]] that just passes all byte
+ * as they are.
+ */
+@TestInstance(Lifecycle.PER_CLASS)
+public class NullMultiNonBlockingTest {
+
+    private final SslContextFactory sslContextFactory = new SslContextFactory();
+    private final SocketPairFactory factory = new SocketPairFactory(sslContextFactory.defaultContext());
+    private final int dataSize = 10 * 1024 * 1024;
+    private final int totalConnections = 50;
+
+    @Test
+    public void testRunTasksInNonBlockingLoop() {
+        Seq<SocketPair> pairs =
+                factory.nioNioN(null, totalConnections, Option.apply(null), true, false, Option.apply(null));
+        NonBlockingLoops.Report report = NonBlockingLoops.loop(pairs, dataSize, false);
+        Assertions.assertEquals(0, report.asyncTasksRun());
+    }
+
+    @AfterAll
+    public void afterAll() {
+        System.out.println(factory.getGlobalAllocationReport());
+    }
+}
